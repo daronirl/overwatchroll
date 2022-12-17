89f734a2c6ab197fb709437bc0d2be5ed4452315
John Prescott <48197513+daronirl@users.noreply.github.com>
2022-12-16 17:21:53 -0800

    Created arrays of heros

---
+++ script.js
@@ -0,0 +1,48 @@
+
+const tankHeros = [
+    'DVA',
+    'Doomfist',
+    'Junker Queen',
+    'Orisa',
+    'Ramattra',
+    'Reinhardt',
+    'Roadhog',     
+    'Sigma',
+    'Winston',
+    'Wrecking Ball',
+    'Zarya'
+    
+];
+
+const damageHeros = [
+    'Ashe',
+    'Bastion',
+    'Cassidy',
+    'Echo',
+    'Genji',
+    'Hanzo',
+    'Junkrat',
+    'Mei',
+    'Pharah',
+    'Reaper',
+    'Sojourn',
+    'Soldier:76',
+    'Sombra',
+    'Symmetra',
+    'TorbJorn',
+    'tracer',
+    'Widow'
+
+];
+
+const healerHeros = [
+    'Ana',
+    'Baptiste',
+    'Brigitte',
+    'Kiriko',
+    'Lucio',
+    'Mercy',
+    'Moira',
+    'Zenyatta'
+
+];
